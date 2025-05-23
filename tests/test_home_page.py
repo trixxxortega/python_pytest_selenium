35d00ff2e74f884e445bfc078f116b010fe74ab1
trixxx.ortega <ortegatrici@gmail.com>
2025-05-23 17:38:58 -0300

    Add screenshot support

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -1,9 +1,9 @@
 from urllib.parse import urljoin
-
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from utils.config import BASE_URL
 from utils.driver_setup import driver
 
@@ -11,22 +11,31 @@ def test_logo_presence_and_clickable(driver):
     url = BASE_URL
     driver.get(url)
 
-    # Wait for the logo to be visible
-    logo = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, '//*[@id="container-bca776d573"]/div[1]/div[2]/div[1]/div[1]/a/img'))
-    )
-    assert logo is not None, "Logo is not present on Home Page"
-
-    # Validate logo is clickable
     try:
-        WebDriverWait(driver, 5).until(
+        # Wait for the logo to be visible
+        logo = WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '//*[@id="container-bca776d573"]/div[1]/div[2]/div[1]/div[1]/a/img'))
+        )
+    except TimeoutException:
+        driver.save_screenshot("error_logo_not_visible.png")
+        pytest.fail("Logo is not present on Home Page")
+
+    try:
+        # Validate logo is clickable
+        WebDriverWait(driver, 20).until(
             EC.element_to_be_clickable((By.XPATH, '//*[@id="container-bca776d573"]/div[1]/div[2]/div[1]/div[1]/a/img'))
         )
-    except:
+    except TimeoutException:
+        driver.save_screenshot("error_logo_not_clickable.png")
         pytest.fail("Logo is not clickable")
 
     # Opcional: clickeamos y validamos URL
     logo.click()
     expected_url = urljoin(BASE_URL, "/en/gl.html")
-    WebDriverWait(driver, 5).until(EC.url_to_be(expected_url))
-    assert driver.current_url == expected_url, "Wrong redirection URL after clicking the logo"
+    try:
+        WebDriverWait(driver, 20).until(EC.url_to_be(expected_url))
+    except TimeoutException:
+        driver.save_screenshot("error_wrong_redirect_url.png")
+        pytest.fail("Wrong redirection URL after clicking the logo")
+
+    assert driver.current_url == expected_url
