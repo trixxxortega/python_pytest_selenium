19571185064a3ff3cfe4eb36f957b10d0e849efe
trixxx.ortega <ortegatrici@gmail.com>
2025-05-23 17:32:22 -0300

    Add support for successful Actions execution

--- utils/driver_setup.py
+++ utils/driver_setup.py
@@ -10,10 +10,13 @@ from utils.config import BASE_URL
 def driver():
     options = Options()
     options.add_argument("--headless=new") # Run Chrome in headless mode (no UI)
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
 
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(10)
+    driver.implicitly_wait(20)
 
     yield driver
 
